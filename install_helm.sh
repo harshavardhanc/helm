76bd9577718f8a9e6fca01a3e6f561639dfe8f9c
harshavardhanc <harshavardhanc95@gmail.com>
2019-10-24 14:49:54 +0530

    helm install

--- install_helm.sh
+++ install_helm.sh
@@ -2,9 +2,7 @@
 
 kubectl -n kube-system create serviceaccount tiller
 
-kubectl create clusterrolebinding tiller \
-  --clusterrole=cluster-admin \
-  --serviceaccount=kube-system:tiller
+kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 
 helm init --service-account tiller
 
